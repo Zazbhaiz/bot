6b3315339c91021e1fd7e196a9f042f205fae487
ʑɑʑɓɦɑɪ <96649732+Zazbhaiz@users.noreply.github.com>
2022-11-20 12:16:10 +0530

    z

---
+++ MARKX/utils/inline/start.py
@@ -0,0 +1,57 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from MARKX import app
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="» Aᴅᴅ ᴍᴇ «",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❍ʜᴇʟᴩ❍",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="❍sᴇᴛᴛɪɴɢs❍", callback_data="settings_helper"
+            ),
+        ],
+        [
+            
+            InlineKeyboardButton(text="❍ᴄʜᴀɴɴᴇʟ❍", url=f"{config.SUPPORT_CHANNEL}"),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="» Aᴅᴅ ᴍᴇ  «",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="❍ʜᴇʟᴩ❍", callback_data="settings_back_helper"
+            ),
+            
+        ],
+        [ 
+            InlineKeyboardButton(text="❍ᴄʜᴀɴɴᴇʟ❍", url=f"{config.SUPPORT_CHANNEL}"),
+            
+        ],
+        
+     ]
+    return buttons
+
+
+
