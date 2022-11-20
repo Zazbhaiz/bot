a0952fa52a24657c17fbb6153ad28a9af9c4a2f9
Zazbhai <96649732+Zazbhaiz@users.noreply.github.com>
2022-11-20 14:36:20 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-     "https://github.com/Golu8709/Mark",
+     "https://github.com/Golu8709/bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
